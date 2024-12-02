e9f5792c83f83ac9d10eb51e3e26a8282071d6b3
[musaew] <[grasjesdraste@yandex.ru]>
2024-12-02 03:50:03 +0300

    added show function and docs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import sys
-from distutils.core import setup
 
 import pyrogram
 import typer
@@ -8,7 +7,6 @@ from pyrogram import Client
 from pyrogram.errors import PeerIdInvalid, ChatIdInvalid
 import hashlib
 import json
-import os
 
 import configparser
 import os
@@ -34,7 +32,6 @@ def setup():
         config.write(configfile)
 
     print(f"Файл конфигурации {config_file} создан с заданными значениями.")
-
 
 try:
     config.read(config_file)
@@ -98,6 +95,12 @@ def send(
     chat: str = typer.Option(None, help="ID чата, префикс хеша или тег."),
     message: str = typer.Argument(..., help="Сообщение для отправки.")
 ):
+    """
+        Отправляет сообщение
+
+        :param chat: Идентификатор чата (id, hash или tag)
+        :param message: Желаемое сообщение
+    """
     client.start()
     try:
         hashes = load_hashes()
@@ -111,17 +114,22 @@ def send(
             # Поиск по префиксу хеша
             chat_id = find_chat_id_by_hash_prefix(hashes, chat)
             if not chat_id:
-                chat_id = get_chat_id(chat_id)
+                chat_id = get_chat_id(chat)
 
         client.send_message(chat_id, message)
-        print("Отправлено")
+        typer.echo("Отправлено")
     except (PeerIdInvalid, ChatIdInvalid) as e:
-        print(f"Ошибка: {e}")
+        typer.echo(f"Ошибка: {e}")
     finally:
         client.stop()
 
 @app.command()
 def get_chats(count: int):
+    """
+        Показывает последние count чатов
+
+        :param count: Количество чатов
+    """
     client.start()
     dialogs = client.get_dialogs(limit=count)
     chat_list = [(hash_id(str(dialog.chat.id)), str(dialog.chat.id), dialog.chat.title or dialog.chat.first_name) for dialog in dialogs]
@@ -131,20 +139,20 @@ def get_chats(count: int):
 
     # Определяем максимальную ширину для каждой колонки
     hash_width = len(chat_list[-1][0])
-    id_width = len(chat_list[-1][1])
+    id_width = 17
     name_width = max(len(chat[2]) for chat in chat_list)
     tag_width = max(len(tag) for tag in tags.keys()) if tags else 0
 
     # Форматируем заголовок
     header = f"{'HASH':<{hash_width}}\t{'ID':<{id_width}}\t{'NAME':<{name_width}}\t{'TAG':<{tag_width}}"
-    print(header)
+    typer.echo(header)
 
     # Форматируем строки
     hashes = {}
     for chat in chat_list:
         hash_val, id_val, name_val = chat
         tag_val = next((tag for tag, chat_id in tags.items() if chat_id == id_val), '')
-        print(f"{hash_val:<{hash_width}}\t{id_val:<{id_width}}\t{name_val:<{name_width}}\t{tag_val:<{tag_width}}")
+        typer.echo(f"{hash_val:<{hash_width}}\t{id_val:<{id_width}}\t{name_val:<{name_width}}\t{tag_val:<{tag_width}}")
         hashes[hash_val] = id_val
 
     save_hashes(hashes)
@@ -152,6 +160,12 @@ def get_chats(count: int):
 
 @app.command()
 def tag(chat: str, tag: str):
+    """
+        Добавляет тег к чату.
+
+        :param chat: Идентификатор чата (id, hash или tag)
+        :param tag: Желаемый тег
+    """
     client.start()
     try:
         hashes = load_hashes()
@@ -169,22 +183,67 @@ def tag(chat: str, tag: str):
 
         tags[tag] = chat_id
         save_tags(tags)
-        print(f"Тег '{tag}' добавлен к чату с ID {chat_id}")
+        typer.echo(f"Тег '{tag}' добавлен к чату с ID {chat_id}")
     except (PeerIdInvalid, ChatIdInvalid) as e:
-        print(f"Ошибка: {e}")
+        typer.echo(f"Ошибка: {e}")
     finally:
         client.stop()
 
 @app.command()
 def get_tags():
+    """
+        Показывает список тегов прикрепленных к чатам.
+    """
     tags = load_tags()
     if tags:
-        print("Список тегов:")
+        typer.echo("Список тегов:")
         for tag, chat_id in tags.items():
-            print(f"{tag}: {chat_id}")
+            typer.echo(f"{tag}: {chat_id}")
     else:
-        print("Нет тегов.")
+        typer.echo("Нет тегов.")
 
+@app.command()
+def show(chat: str, count: int):
+    """
+    Показывает последние n сообщений из указанного чата.
+
+    :param chat: Идентификатор чата (id, hash или tag)
+    :param count: Количество сообщений для отображения
+    """
+    client.start()
+    try:
+        hashes = load_hashes()
+        tags = load_tags()
+
+        if chat in hashes:
+            chat_id = hashes[chat]
+        elif chat in tags:
+            chat_id = tags[chat]
+        else:
+            # Поиск по префиксу хеша
+            chat_id = find_chat_id_by_hash_prefix(hashes, chat)
+            if not chat_id:
+                chat_id = get_chat_id(chat)
+
+        # Получаем последние n сообщений
+        messages = client.get_chat_history(chat_id=chat_id, limit=count)
+
+        # Отображаем сообщения
+        for msg in list(messages)[::-1]:
+            text = msg.text or msg.caption or "No text"
+            if msg.from_user.last_name:
+                name = msg.from_user.first_name + msg.from_user.last_name
+            else:
+                name = msg.from_user.first_name
+
+            typer.echo(f"{name}\nText:")
+            typer.echo(f"{text}\n")
+            typer.echo(f"| {msg.date} |")
+            typer.echo(f"- - - - - - - - - - - - - - - - - - - - - -")
+    except (PeerIdInvalid, ChatIdInvalid) as e:
+        typer.echo(f"Ошибка: {e}")
+    finally:
+        client.stop()
 
 if __name__ == "__main__":
     app()
